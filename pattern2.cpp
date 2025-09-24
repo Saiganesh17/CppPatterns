4db0014e869ae20087c0ee462f91ab7e282d4402
Sai Ganesh Reddy <109289610+Saiganesh17@users.noreply.github.com>
2025-09-24 11:15:07 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void pattern2(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<< " ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout<< "*";
+        }
+        for(int j=0;j<n-i-1; j++){
+            cout<< " ";
+        }
+        cout << endl; // Added newline after each row
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    pattern2(n); 
+    return 0;
+}
